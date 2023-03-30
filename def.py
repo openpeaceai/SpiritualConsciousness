9bfeefcabac902c363b51f3ffd627d8870463d07
openpeaceai <father@openpeace.ai>
2023-03-29 21:45:08 -0700

    single column update

--- def.py
+++ def.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[43]:
 
 
 import streamlit as st
 
 
-# In[2]:
+# In[50]:
 
 
 st.set_page_config(
-    page_title="Spiritual Consciousness Survey",
+    page_title="Spiritual Consciousness Assesment",
     page_icon="☮️",
     layout="centered",
     initial_sidebar_state="expanded",
@@ -21,19 +21,69 @@ st.set_page_config(
 )
 
 
+# In[101]:
+
+
+# Add css to make text bigger
+st.markdown(
+    """
+    <style>
+    
+    label 
+    {
+    font-size: 3rem !important;
+    color: #228B22;
+    }
+    
+    div[class*="stSelect"] label {
+    font-size: 26px;
+    color: #228B22;
+    }
+
+
+    div[class*="stTextArea"] label {
+    font-size: 3rem !important;
+    color: #228B22;
+    }
+
+    div[class*="stTextInput"] label {
+    font-size: 3rem !important;
+    color: #228B22;
+    }
+    
+    div[class*="stDateInput"] label {
+    font-size: 3rem !important;
+    color: #228B22;
+    }
+
+    div[class*="stNumberInput"] label {
+    font-size: 3rem !important;
+    color: #228B22;
+    }
+    
+    div[data-testid="column"]:nth-of-type(1)
+    {
+        border:0px solid blue;
+    }
+    div[data-testid="column"]:nth-of-type(2)
+    {
+        border:0px solid blue;
+        text-align: end;
+    }
+        
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
 # In[ ]:
 
 
 
 
 
-# In[ ]:
-
-
-
-
-
-# In[3]:
+# In[52]:
 
 
 import pandas as pd
@@ -52,16 +102,23 @@ from datetime import datetime, date, timedelta
 
 
 
-# In[4]:
+# In[94]:
 
 
-with st.sidebar:
-    st.image("https://img1.wsimg.com/isteam/ip/f8df9fda-2223-42be-a383-5d7d72e7c082/Openpeace%20Logo_Layout%201A.png/:/rs=w:230,h:38,cg:true,m/cr=w:230,h:38/qt=q:100/ll", width=230)  # Adjust the width as needed
-    st.header("Spiritual Consciousness Survey")
-    st.write("Introducing the Spiritual Consciousness Survey, a unique and engaging                 questionnaire designed to explore individual perspectives on spirituality,                 personal growth, and transcendent experiences.")
-    
-
-st.subheader("Please enter your email, birthday and country:")    
+#with st.sidebar:
+st.image("https://img1.wsimg.com/isteam/ip/f8df9fda-2223-42be-a383-5d7d72e7c082/Openpeace%20Logo_Layout%201A.png/:/rs=w:230,h:38,cg:true,m/cr=w:230,h:38/qt=q:100/ll", width=230)  # Adjust the width as needed
+st.header("Spiritual Consciousness Assesment")
+st.markdown('##### The Spiritual Consciousness Assesment is a unique and engaging \
+            questionnaire designed to explore individual perspectives on spirituality, \
+            personal growth, and transcendent experiences.')
+#st.markdown('##') 
+st.markdown('##### Use the form below to select the answers to the questions presented and submit it to see your Spiritual \
+            Consciousness score.')
+st.markdown('---')     
+st.markdown('##### Please enter your email address, date of birth, and country.')
+st.markdown('###### :red[Note that you can only take this assessment once, and your information \
+                will remain anonymous, as we prioritize your privacy and security.]', unsafe_allow_html=False)
+#st.write("Please enter your email, birthday and country:")    
 # Input fields for email, birthday and country
 email = st.text_input("Email:", "")
 min_birthday = date.today() - timedelta(days=365*110)
@@ -69,8 +126,10 @@ max_birthday = date.today() - timedelta(days=365*14)
 birthday = st.date_input("Birthday (MM/DD/YYYY):", value=datetime(2000, 1, 1), min_value=min_birthday, max_value=max_birthday)
 countries = ["Select Country"] + sorted(["United States"] + [country.name for country in pycountry.countries if country.name != "United States"])
 country = st.selectbox("Country:", countries)
-    
-st.subheader("About your spiritual consciousness:")
+st.markdown('---')     
+st.markdown('##### The following section explores a range of topics, \
+            including beliefs, practices, interconnectedness, transcendent experiences, \
+            and personal development.', unsafe_allow_html=False)
 
 
 # In[ ]:
@@ -79,7 +138,7 @@ st.subheader("About your spiritual consciousness:")
 
 
 
-# In[5]:
+# In[82]:
 
 
 server = '184.168.194.64'
@@ -100,7 +159,7 @@ engine = create_engine(connection_str)
 
 
 
-# In[6]:
+# In[83]:
 
 
 # Fetch questions and sections from the database
@@ -120,7 +179,7 @@ answers_tuples = list(answers.itertuples(index=False))
 
 
 
-# In[7]:
+# In[84]:
 
 
 options_dict = {}
@@ -137,7 +196,7 @@ for question_id, answer_text, answer_value in answers_tuples:
 
 
 
-# In[8]:
+# In[88]:
 
 
 responses = []
@@ -145,7 +204,8 @@ responses = []
 current_section = None
 for question_id, question_text, question_section in questions_tuples:
     if current_section != question_section:
-        st.subheader(question_section)
+        st.markdown('---')
+        st.markdown(f"##### {question_section}", unsafe_allow_html=False)
         current_section = question_section
 
     response = st.selectbox(
@@ -153,8 +213,10 @@ for question_id, question_text, question_section in questions_tuples:
         options=[(text, value) for text, value in options_dict[question_id]],
         format_func=lambda option: option[0],
         key=question_id
+        
     )
     responses.append(response[1])  # Store the answer value
+    
 
 
 # In[ ]:
@@ -163,7 +225,7 @@ for question_id, question_text, question_section in questions_tuples:
 
 
 
-# In[9]:
+# In[58]:
 
 
 # Active user flag
@@ -178,8 +240,10 @@ survey_id = 1  # You can change this to the ID of the desired survey
 
 
 
-# In[10]:
+# In[98]:
 
+
+st.markdown('##')
 
 if st.button("Submit"):
       
@@ -230,16 +294,34 @@ if st.button("Submit"):
                 query = f"INSERT INTO spiritual_consciousness_user_survey_score (user_id, survey_id, score) VALUES ({user_id}, {survey_id}, {total_percentage_of_user_submission})"
                 engine.execute(query)
 
-                st.sidebar.success("Thank you for your submission!")
-                st.sidebar.info(f"Your spiritual consciousness is at the : {total_percentage_of_user_submission:.2f}%")
+                st.success("Thank you for your submission!")
+                st.info(f"Your spiritual consciousness is at the : {total_percentage_of_user_submission:.2f}%")
                 st.balloons()
                 
             else:
-                st.sidebar.warning("You have already submitted this survey. You cannot submit it more than once.")
+                st.warning("You have already submitted this survey. You cannot submit it more than once.")
         else:
-            st.sidebar.error("Please provide all required information.")     
+            st.error("Please provide all required information.")     
     else:
-        st.sidebar.error("Please answer all questions before submitting the survey.")
+        st.error("Please answer all questions before submitting the survey.")
+
+
+# In[99]:
+
+
+st.markdown('##')
+st.markdown('---')
+
+
+# In[100]:
+
+
+col1, col2 = st.columns(2)
+with col1:
+   st.markdown(' :gray[2023 - openpeace.ai, all rights reserved.]', unsafe_allow_html=False)
+
+with col2:
+   st.markdown(' :gray[v - 0.1]', unsafe_allow_html=False)
 
 
 # In[ ]:
